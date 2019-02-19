3e76fca2b8aa4bbcbd6a481d1e56e386addc85d3
Deividas Gedgaudas <sidicer@gmail.com>
2019-02-19 19:11:19 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -12,34 +12,40 @@ private:
 	int centroX,
 		centroY,
 		skersmuo;
-	float spindulys;
+	double spindulys;
 public:
 	int getCentroX() { return this->centroX; }
 	int getCentroY() { return this->centroY; }
 	int getSkersmuo() { return this->skersmuo; }
-	float getSpindulys() { return this->spindulys; }
+	double getSpindulys() { return this->spindulys; }
 
-	void set(int centroX, int centroY, int skersmuo, float spindulys) { 
+	void set(int centroX, int centroY, int skersmuo, double spindulys) {
 		this->centroX = centroX; this->centroY = centroY; this->skersmuo = skersmuo; this->spindulys = spindulys;
 	}
 	void setCentroX(int centroX) { this->centroX = centroX; }
 	void setCentroY(int centroY) { this->centroY = centroY; }
 	void setSkersmuo(int skersmuo) { this->skersmuo = skersmuo; }
-	void setSpindulys(float spindulys) { this->spindulys = spindulys; }
+	void setSpindulys(double spindulys) { this->spindulys = spindulys; }
 };
 
 int main()
 {
 	skrituliai Skritulys[100];
-	int n, k;
+	int n, k(0);
 	std::cin >> n;
 	for (int i = 0; i < n; i++) {
 		int x, y, d;
 		std::cin >> x >> y >> d;
-		Skritulys[i].set(x, y, d, (float)d/2);
+		Skritulys[i].set(x, y, d, (double)d / 2);
 	}
 
 	for (int i = 0; i < n; i++) {
+		double atstumas(0);
+		atstumas = sqrt( pow(Skritulys[i].getCentroX(),2) + pow(Skritulys[i].getCentroY(),2) );
 
+		if (atstumas <= Skritulys[i].getSpindulys())
+			k++;
 	}
+
+	printf("%i", k);
 }
